1b06a07429b449567335846cb12075a0af9ca9d2
HMwancheng <91036325+HMwancheng@users.noreply.github.com>
2025-11-25 21:23:09 +0800

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") } // 用于lyric-parser库
+    }
+}
+
+rootProject.name = "LyricFloat"
+include(":app")
