243304422d37e6260508d3fd81d884231be8b01b
HMwancheng <91036325+HMwancheng@users.noreply.github.com>
2025-11-27 13:18:12 +0800

    Update LyricFloatApp.kt

--- app/src/main/kotlin/com/lyricfloat/LyricFloatApp.kt
+++ app/src/main/kotlin/com/lyricfloat/LyricFloatApp.kt
@@ -2,10 +2,16 @@ package com.lyricfloat
 
 import android.app.Application
 
-// 改为普通Application（Android 10+无需MultiDexApplication）
 class LyricFloatApp : Application() {
+    var mainActivity: MainActivity? = null
+    
     override fun onCreate() {
         super.onCreate()
-        // 移除MultiDex.install()及相关代码
+        instance = this
+    }
+    
+    companion object {
+        lateinit var instance: LyricFloatApp
+            private set
     }
 }
